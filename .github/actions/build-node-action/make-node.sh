babd3023ebca4930f6c14a850519507f01c25677
Maurits Lamers <mauritslamers@sproutcore.com>
2026-01-29 14:13:07 +0100

    bring in libstdc++

--- .github/actions/build-node-action/make-node.sh
+++ .github/actions/build-node-action/make-node.sh
@@ -34,8 +34,29 @@ tar -xvzf ${GITHUB_WORKSPACE}/${NODE_DIR}/node-${NODE_VERSION}-linux-x64.tar.gz
 
 cd node-${NODE_VERSION}-linux-x64
 cp -R /lib64 ./lib64
+
+# Ensure we don't accidentally pick up the host's libstdc++/libgcc_s at runtime.
+# These come from devtoolset-9 (built for EL6) and should be more compatible than
+# whatever happens to be installed on the target system (e.g. Rocky).
+LIBSTDCXX_PATH=$(scl enable devtoolset-9 "g++ -print-file-name=libstdc++.so.6")
+LIBGCC_PATH=$(scl enable devtoolset-9 "gcc -print-file-name=libgcc_s.so.1")
+
+cp -L "${LIBSTDCXX_PATH}" ./lib64/
+cp -L "${LIBGCC_PATH}" ./lib64/
+
+# Optional but commonly needed with modern toolchains.
+LIBATOMIC_PATH=$(scl enable devtoolset-9 "gcc -print-file-name=libatomic.so.1")
+if [ -f "${LIBATOMIC_PATH}" ] && [ "${LIBATOMIC_PATH}" != "libatomic.so.1" ]; then
+	cp -L "${LIBATOMIC_PATH}" ./lib64/
+fi
+
+LIBGOMP_PATH=$(scl enable devtoolset-9 "gcc -print-file-name=libgomp.so.1")
+if [ -f "${LIBGOMP_PATH}" ] && [ "${LIBGOMP_PATH}" != "libgomp.so.1" ]; then
+	cp -L "${LIBGOMP_PATH}" ./lib64/
+fi
+
 cd bin
-../../bin/patchelf --set-interpreter ../lib64/ld-linux-x86-64.so.2 --set-rpath ../lib64 node 
+../../bin/patchelf --set-interpreter ../lib64/ld-linux-x86-64.so.2 --set-rpath '$ORIGIN/../lib64' node 
 
 ## now repack
 cd ${GITHUB_WORKSPACE}/centos_patch
